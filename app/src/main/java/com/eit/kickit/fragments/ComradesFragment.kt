4750d3023d5460251298d9a8f45e9e2c9dfff6e2
Thomas Adams <tomad212@gmail.com>
2019-07-23 14:55:11 +0200

    Initial App Commit

---
+++ app/src/main/java/com/eit/kickit/fragments/ComradesFragment.kt
@@ -0,0 +1,16 @@
+package com.eit.kickit.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.eit.kickit.R
+
+class ComradesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return LayoutInflater.from(container?.context).inflate(R.layout.fragment_comrades, container, false)
+    }
+
+}
