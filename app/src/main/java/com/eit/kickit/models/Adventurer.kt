3c1106dfd75887a176429ce5ca6ba10bd362ad8f
Thomas Adams <tomad212@gmail.com>
2019-08-02 17:29:56 +0200

    Added more things

---
+++ app/src/main/java/com/eit/kickit/models/Adventurer.kt
@@ -0,0 +1,10 @@
+package com.eit.kickit.models
+
+data class Adventurer (
+    val avdID: Int,
+    val advFirstName: String,
+    val advSurname: String,
+    val advEmail: String,
+    val advTelephone: String,
+    val advActive: Boolean,
+    val advAdmin: Boolean)
