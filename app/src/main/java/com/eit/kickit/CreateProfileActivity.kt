126fb478849f0e09ce7007cf13bb45c9274bf44b
Thomas Adams <tomad212@gmail.com>
2019-08-06 09:26:05 +0200

    Created Validation Class

--- app/src/main/java/com/eit/kickit/CreateProfileActivity.kt
+++ app/src/main/java/com/eit/kickit/CreateProfileActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.eit.kickit.common.Validator
 import com.eit.kickit.database.DatabaseController
 import com.eit.kickit.models.Adventurer
 import com.google.android.material.textfield.TextInputLayout
@@ -40,6 +42,10 @@ class CreateProfileActivity : AppCompatActivity() {
 
         if(validation()){
             //DatabaseController.createAdventurer(); --Return an adventurer object.
+            Toast.makeText(this, "Valid, Do Da Tings", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "Please enter all the fields correctly", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -52,97 +58,13 @@ class CreateProfileActivity : AppCompatActivity() {
         var pValid = false
         var rtpValid = false
 
-        fnValid = validateView(fn_textInput, firstname, 1)
-        snValid = validateView(sn_textInput, surname, 1)
-        emValid = validateView(em_textInput, Email, 2)
-        tpValid = validateView(tn_textInput, phone, 3)
-        pValid = validateView(pw_textInput, pword, 4)
-        rtpValid = validateView(pwr_textInput, rtpword, 5)
+        fnValid = Validator.validateView(fn_textInput, firstname, 1)
+        snValid = Validator.validateView(sn_textInput, surname, 1)
+        emValid = Validator.validateView(em_textInput, Email, 2)
+        tpValid = Validator.validateView(tn_textInput, phone, 3)
+        pValid = Validator.validateView(pw_textInput, pword, 4)
+        rtpValid = Validator.validateView(pwr_textInput, rtpword, 1) && rtpword.equals(pword)
 
-        return true
-
-    }
-
-    fun validateView(view: TextInputLayout, text: String, type: Int): Boolean{
-
-
-        var result = false
-
-        when(type){
-            1 -> {
-                if(text.isNotBlank()){
-                    view.helperText = ""
-                    result = true
-                }
-                else{
-                    view.helperText = "Required"
-                    result = false
-                }
-            }
-
-            2 -> {
-                result = validateView(view, text, 1)
-                if (result){
-                    if (android.util.Patterns.EMAIL_ADDRESS.matcher(text).matches()){
-                        result = false
-                        view.helperText = ""
-                    }
-                    else{
-                        result = true
-                        view.helperText = "Enter Valid Email"
-                    }
-
-                }
-            }
-
-            3 -> {
-                result = validateView(view, text, 1)
-                if (result){
-                    if(android.util.Patterns.PHONE.matcher(text).matches() && (text.length == 10)){
-                        result = true
-                        view.helperText = ""
-                    }
-                    else {
-                        result = false
-                        view.helperText = "Enter Valid Phone Number"
-                    }
-                }
-            }
-
-            4 -> {
-                result = validateView(view, text, 1)
-                //Longer than 8 characters
-                //One number
-                val check = "a[a-z0-9]+d?".toRegex()
-
-                if(result){
-                    if(check.containsMatchIn(text) && (text.length >= 8)){
-                        result = true
-                        view.helperText = ""
-                    }
-                    else {
-                        result = false
-                        view.helperText = "Must contain a number, a letter and be longer than 8 characters"
-                    }
-                }
-            }
-
-            5 -> {
-                result = validateView(view, text, 1)
-
-                if(result){
-                    if (text.equals(pword)){
-                        result = true
-                        view.helperText = ""
-                    }
-                    else {
-                        result = false
-                        view.helperText = "Passwords Don't match"
-                    }
-                }
-            }
-        }
-
-        return result
+        return fnValid && snValid && emValid && tpValid && pValid && rtpValid
     }
 }
