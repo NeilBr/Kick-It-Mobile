9b66691db4b64b340447f359815988d1359ad404
Daniel <mcbeydaniel1240@gmail.com>
2019-08-15 23:17:22 +0200

    Flow of bucket branch fixed
    Shit hit the fan. Did something bad, reverted too far back. Cried in corner and recoded all the working code I lost.

--- app/src/main/java/com/eit/kickit/activities/ViewChallengeActivity.kt
+++ app/src/main/java/com/eit/kickit/activities/ViewChallengeActivity.kt
@@ -1,6 +1,8 @@
 package com.eit.kickit.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.eit.kickit.R
 
@@ -8,5 +10,10 @@ class ViewChallengeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_challenge)
+    }
+
+    fun onAddClick(view : View)
+    {
+        Toast.makeText(this,"Added to My BucketList", Toast.LENGTH_SHORT).show()
     }
 }
