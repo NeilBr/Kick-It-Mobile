6546a0a810cbe2e05e3a68f212f0a3f73c9f8f09
Daniel <mcbeydaniel1240@gmail.com>
2019-09-28 17:07:25 +0200

    Suggest challenge use case
    New database table
    UI for use case
    Database connection for use case

--- app/src/main/java/com/eit/kickit/fragments/SuggestChallengeFragment.kt
+++ app/src/main/java/com/eit/kickit/fragments/SuggestChallengeFragment.kt
@@ -1,16 +1,146 @@
 package com.eit.kickit.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.eit.kickit.R
+import com.eit.kickit.database.Database
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.android.synthetic.main.fragment_suggest_challenge.*
+import java.sql.ResultSet
 
-class SuggestChallengeFragment : Fragment() {
+class SuggestChallengeFragment : Fragment(){
+
+    private var txtName : TextInputEditText? = null
+    private var txtDesc : TextInputEditText? = null
+    private var txtPoints : TextInputEditText? = null
+    private var txtPrice : TextInputEditText? = null
+    private var spinBucket : Spinner? = null
+
+    private var scID : Int = 0
+    private var scName : String = ""
+    private var scDesc : String = ""
+    private var scPoints : Int = 0
+    private var scPrice : Double = 0.00
+    private var scBucket : String = ""
+
+    private val bucketlists : ArrayList<String> = ArrayList()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return LayoutInflater.from(container?.context).inflate(R.layout.fragment_suggest_challenge, container, false)
+        val view = inflater.inflate(R.layout.fragment_suggest_challenge, container, false)
+
+        val btnSuggestChallenge = view.findViewById<Button>(R.id.btnSuggest)
+        btnSuggestChallenge.setOnClickListener{
+            suggestChallenge()
+        }
+
+        val btnCancel = view.findViewById<Button>(R.id.btnCancel)
+        btnCancel.setOnClickListener {
+            cancelSuggestion()
+        }
+
+        txtName = view.findViewById(R.id.challengeName)
+        txtDesc = view.findViewById(R.id.challengeDescription)
+        txtPoints = view.findViewById(R.id.challengePoints)
+        txtPrice = view.findViewById(R.id.challengePrice)
+        spinBucket = view.findViewById(R.id.bucketSpinner)
+
+        val query = "SELECT * FROM bucketlists"
+        Database().runQuery(query, true)
+        {
+            result -> loadBucketChoices(result)
+        }
+
+        return view
     }
 
+    private fun loadBucketChoices(result: Any)
+    {
+        val resultSet: ResultSet = result as ResultSet
+
+        while(resultSet.next())
+        {
+            bucketlists.add(resultSet.getString("bl_name"))
+        }
+
+        spinBucket?.onItemSelectedListener
+        spinBucket!!.adapter = ArrayAdapter(activity!!.applicationContext, R.layout.support_simple_spinner_dropdown_item, bucketlists)
+        spinBucket?.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val type = parent?.getItemAtPosition(position).toString()
+                scBucket = type
+            }
+
+        }
+    }
+
+
+    private fun suggestChallenge()
+    {
+        val query = "SELECT * FROM suggested_challenges"
+
+        Database().runQuery(query, true)
+        {
+            result -> findLastSuggestion(result)
+        }
+
+    }
+
+    private fun findLastSuggestion(result : Any)
+    {
+        val resultSet: ResultSet = result as ResultSet
+
+        while (resultSet.next())
+        {
+            scID++
+        }
+
+        progressBarSuggest.visibility = View.VISIBLE
+        postSuggestion()
+    }
+
+    private fun postSuggestion()
+    {
+        scName = txtName!!.text.toString()
+        scDesc = txtDesc!!.text.toString()
+        scPoints = Integer.parseInt(txtPoints!!.text.toString())
+        //scPrice =
+
+        val query = "INSERT INTO suggested_challenges VALUES($scID,'$scName','$scDesc',$scPoints,$scPrice,'$scBucket')"
+
+        Database().runQuery(query, false)
+        {
+            result -> sendSuggestion(result)
+        }
+    }
+
+    private fun sendSuggestion(result : Any)
+    {
+        if (result is String)
+        {
+            Toast.makeText(this@SuggestChallengeFragment.context,result, Toast.LENGTH_LONG).show()
+        }
+        else
+        {
+            Toast.makeText(this@SuggestChallengeFragment.context,"Suggestion sent", Toast.LENGTH_SHORT).show()
+        }
+
+        progressBarSuggest.visibility = View.INVISIBLE
+    }
+
+    private fun cancelSuggestion()
+    {
+        challengeName.setText("")
+        challengeDescription.setText("")
+        challengePoints.setText("")
+        challengePrice.setText("")
+    }
 }
+
